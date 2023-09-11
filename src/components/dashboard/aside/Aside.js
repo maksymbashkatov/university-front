e620beb104db92a4501b9163a5096abbb46aabb0
Maksym Bashkatov <maksymbashkatov@gmail.com>
2023-09-12 00:02:44 +0300

    Was added aside feature.

--- src/components/dashboard/aside/Aside.js
+++ src/components/dashboard/aside/Aside.js
@@ -1,11 +1,13 @@
 import './aside.scss';
+import Nav from './nav/Nav';
 
-export default function Aside(props) {
+export default function Aside() {
   return (
     <aside className="aside">
       <div className="aside__logo container">
         <img src="/images/header-logo.png" alt="logo" />
       </div>
+      <Nav />
     </aside>
   );
 }

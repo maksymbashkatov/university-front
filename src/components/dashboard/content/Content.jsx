ff89b6e856d6ef56392937f646a94792023caf20
Maksym Bashkatov <maksymbashkatov@gmail.com>
2023-10-15 13:17:02 +0300

    Add simple list structure for lectors.

--- src/components/dashboard/content/Content.jsx
+++ src/components/dashboard/content/Content.jsx
@@ -1,15 +1,69 @@
+import { useEffect, useState } from 'react';
 import './content.scss';
 
+function Lector(props) {
+  return (
+    <li>
+      {props.name}, {props.surname}, {props.email}
+    </li>
+  );
+}
+
+const LECTORS = [
+  {
+    id: 1,
+    name: 'Name1',
+    surname: 'Surname1',
+    email: 'email1@mail.com',
+  },
+  {
+    id: 2,
+    name: 'Name2',
+    surname: 'Surname2',
+    email: 'email2@mail.com',
+  },
+  {
+    id: 3,
+    name: 'Name3',
+    surname: 'Surname3',
+    email: 'email3@mail.com',
+  },
+];
+
 export default function Content(props) {
-  const { h1, personPhotoSrc } = props;
+  const [lectors, setLectors] = useState([]);
+  useEffect(() => {
+    setLectors(LECTORS);
+  }, []);
 
   return (
     <main className="main">
+      <button
+        onClick={(e) => {
+          setLectors([
+            ...lectors,
+            {
+              id: 4,
+              name: 'Name4',
+              surname: 'Surname4',
+              email: 'email3@mail.com',
+            },
+          ]);
+          console.log(lectors);
+        }}
+      >
+        Creat Lector
+      </button>
       <ul>
-        <li>List Title</li>
-        <li>List Item1</li>
-        <li>List Item2</li>
-        <li>List Item3</li>
+        <li>Name, Surname, Email</li>
+        {lectors.map((lector) => (
+          <Lector
+            key={lector.id}
+            name={lector.name}
+            surname={lector.surname}
+            email={lector.email}
+          />
+        ))}
       </ul>
     </main>
   );
